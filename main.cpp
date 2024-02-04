a2372b2afd43836562e3b79e556843af8c6eca2f
Kevin Connelly <kevinconnelly1337@gmail.com>
2024-02-04 11:08:39 -0800

    spacing

--- main.cpp
+++ main.cpp
@@ -7,6 +7,24 @@
 #include"VBO.h"
 #include"EBO.h"
 
+// Define the vertices of the triangle
+GLfloat vertices[] =
+{
+	-0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f, //Lower left
+	0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f, // Lower right
+	0.0f, 0.5f * float(sqrt(3)) * 2 / 3, 0.0f, // Upeer corner
+	-0.5f / 2, 0.5f * float(sqrt(3)) / 6, 0.0f, // Inner left
+	0.5f / 2, 0.5f * float(sqrt(3)) / 6, 0.0f, // Inner right
+	0.0f, -0.5f * float(sqrt(3)) / 3, 0.0f // Inner down
+};
+
+GLuint indices[] =
+{
+	0, 3, 5, // Lower left triangle
+	3, 2, 4, // Lower right triangle
+	5, 4, 1 // Upper triangle
+};
+
 int main() 
 {
 	//Initialize GLFW
@@ -17,24 +35,6 @@ int main()
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	// Tell GLFW we're using the core profile
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-
-	// Define the vertices of the triangle
-	GLfloat vertices[] =
-	{
-		-0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f, //Lower left
-		0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f, // Lower right
-		0.0f, 0.5f * float(sqrt(3)) * 2 / 3, 0.0f, // Upeer corner
-		-0.5f / 2, 0.5f * float(sqrt(3)) / 6, 0.0f, // Inner left
-		0.5f / 2, 0.5f * float(sqrt(3)) / 6, 0.0f, // Inner right
-		0.0f, -0.5f * float(sqrt(3)) / 3, 0.0f // Inner down
-	};
-
-	GLuint indices[] =
-	{
-		0, 3, 5, // Lower left triangle
-		3, 2, 4, // Lower right triangle
-		5, 4, 1 // Upper triangle
-	};
 
 	// Create window and specify size, name
 	GLFWwindow* window = glfwCreateWindow(800, 800, "YoutubeOpenGL", NULL, NULL);
@@ -54,7 +54,6 @@ int main()
 	// Load GLAD so it configures OpenGL
 	gladLoadGL();
 
-
 	// Specify the viewport of OpenGL and its size
 	glViewport(0, 0, 800, 800);
 
@@ -70,46 +69,6 @@ int main()
 	VAO1.Unbind();
 	VBO1.Unbind();
 	EBO1.Unbind();
-
-	// Create reference containers for the vertex array object and the vertex buffer object
-	GLuint VAO, VBO, EBO;
-
-	// Generate the VAO and VBO with 1 object each
-	glGenVertexArrays(1, &VAO);
-	glGenBuffers(1, &VBO);
-	glGenBuffers(1, &EBO);
-
-
-	//Make the VAO the current vertex array object by binding it
-	glBindVertexArray(VAO);
-	// Bind the VBO specifiying its a GL_ARRAY_BUFFER
-	glBindBuffer(GL_ARRAY_BUFFER, VBO);
-
-	// Introduce the vertices into the VBO
-	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
-
-	// Configure the vertex attribute so OpenGL knows how to read the VBO
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
-	// Enable the vertex attribute so that opengl knows how to use it
-	glEnableVertexAttribArray(0);
-
-
-	// Bind the VAO and VBO to 0 so we dont accidentally modify them
-	glBindBuffer(GL_ARRAY_BUFFER, 0);
-	glBindVertexArray(0);
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
-
-	// Specify background color
-	glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
-
-	// Clear the back buffer and assign new color to it
-	glClear(GL_COLOR_BUFFER_BIT);
-
-	// Swap back buffer with front buffer
-	glfwSwapBuffers(window);
 
 	// Main while loop
 	while (!glfwWindowShouldClose(window))
@@ -127,7 +86,6 @@ int main()
 		// Have GLFW handle all events
 		glfwPollEvents();
 	}
-
 
 	// Delete all the objects we created
 	VAO1.Delete();
