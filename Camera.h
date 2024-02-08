975ad8e6c04cae419f3f5d5748cc42ec6da63e68
Kevin Connelly <kevinconnelly1337@gmail.com>
2024-02-07 16:30:06 -0800

    add camera and lighting elements

--- Camera.h
+++ Camera.h
@@ -17,6 +17,7 @@ class Camera
 		glm::vec3 Position;
 		glm::vec3 Orientation = glm::vec3(0.0f, 0.0f, -1.0f);
 		glm::vec3 Up = glm::vec3(0.0f, 1.0f, 0.0f);
+		glm::mat4 cameraMatrix = glm::mat4(1.0f);
 
 		bool firstClick = true;
 
@@ -28,6 +29,7 @@ class Camera
 
 		Camera(int width, int hieght, glm::vec3 position);
 
+		void updateMatrix(float FOVdeg, float nearPlane, float farPlane);
 		void Matrix(float FOVdeg, float nearPlane, float farPlane, Shader& shader, const char* uniform);
 		void Inputs(GLFWwindow* window);
 };	
