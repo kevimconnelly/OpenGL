8b6b44ff0840ed32ae340144007365ca532ef77d
Kevin Connelly <kevinconnelly1337@gmail.com>
2024-02-13 21:23:17 -0800

    add getFloats to model.cpp

--- Model.cpp
+++ Model.cpp
@@ -21,3 +21,35 @@ std::vector<unsigned char> Model::getData()
 	std::vector<unsigned char> data(bytesText.begin(), bytesText.end());
 	return data;
 }
+
+std::vector<float> Model::getFloats(json accessor)
+{
+	std::vector<float> floatVec;
+
+	unsigned int buffViewInd = accessor.value("bufferView", 1);
+	unsigned int count = accessor["count"];
+	unsigned int accByteOffset = accessor.value("byteOffset", 0);
+	std::string type = accessor["type"];
+
+	json bufferView = JSON["bufferViews"][buffViewInd];
+	unsigned int byteOffset = bufferView["byteOffset"];
+
+	unsigned int numPerVert;
+	if (type == "SCALAR") numPerVert = 1;
+	else if (type == "VEC2") numPerVert = 2;
+	else if (type == "VEC3") numPerVert = 3;
+	else if (type == "VEC4") numPerVert = 4;
+	else throw std::invalid_argument("Type is invalid (not SLARA, VEC2, VEC3, or VEC4");
+
+	unsigned int beginningOfData = byteOffset + accByteOffset;
+	unsigned int lengthOfData = count * 4 * numPerVert;
+	for (unsigned int i = beginningOfData; i < beginningOfData + lengthOfData; i)
+	{
+		unsigned char bytes[] = { data[i++], data[i++], data[i++], data[i++] };
+		float value;
+		std::memcpy(&value, bytes, sizeof(float));
+		floatVec.push_back(value);	
+	}
+
+	return floatVec;
+}
--- Model.h
+++ Model.h
@@ -19,6 +19,12 @@ private:
 	json JSON;
 
 	std::vector<unsigned char> getData();
+	std::vector<float> getFloats(json accessor);
+	std::vector<GLuint> getIndices(json accessor);
+
+	std::vector<glm::vec2> groupFloatsVec2(std::vector<float> floatVec);
+	std::vector<glm::vec3> groupFloatsVec3(std::vector<float> floatVec);
+	std::vector<glm::vec4> groupFloatsVec4(std::vector<float> floatVec);
 };
 
 #endif
